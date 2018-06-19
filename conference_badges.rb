e8fbaa940a3061209bcae103ce80c6e5100a541d
Emily Wessel <ettl02@gmail.com>
2018-06-19 00:56:11 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ end
 def assign_rooms (speakers)
   new_arr = []
   speakers.each_with_index do |name, index|
-    new_arr << "Hello, #{name}!, You'll be assigned to room #{index + 1}!"}
+    new_arr << "Hello, #{name}!, You'll be assigned to room #{index + 1}!"
   end
   new_arr
 end
