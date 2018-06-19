ce1d1223a9b30c67c0a88491608ee0b1027048ba
Emily Wessel <ettl02@gmail.com>
2018-06-19 00:44:26 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,16 +4,16 @@ end
 
 def batch_badge_creator(names)
   names.map{ |name| badge_maker(name) }   # initialize greetings as an empty array
-#map return a new array with elements from the badge_maker iterator 
-end 
+#map return a new array with elements from the badge_maker iterator
+end
 
 def assign_rooms (speakers)
   new_arr = []
   speakers.each_with_index do |name, index|
     new_arr << "Hello, #{name}!, You'll be assigned to room #{index + 1}!"}
-
-end
-
+  end
+  new_arr
+end 
 
     puts "Hello, #{speaker}! You'll be assigned to room #{room_number}!"
 #array of room assignments
